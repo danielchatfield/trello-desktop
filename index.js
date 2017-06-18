a62ca158a4183b43410033529433525d98cbe00b
Yuri Vecchi <contato@yurivecchi.com.br>
2017-06-18 16:21:08 -0300

    Hello Travis, my old friend

--- index.js
+++ index.js
@@ -14,122 +14,121 @@ let mainWindow;
 let isQuitting = false;
 
 function createMainWindow() {
-	const lastWindowState = config.get('lastWindowState');
-	const win = new electron.BrowserWindow({
-		title: app.getName(),
-		show: false,
-		x: lastWindowState.x,
-		y: lastWindowState.y,
-		width: lastWindowState.width,
-		height: lastWindowState.height,
-		icon: process.platform === 'linux' && path.join(__dirname, 'static', 'Icon.png'),
-		minWidth: 400,
-		minHeight: 200,
-		titleBarStyle: 'hidden-inset',
-		autoHideMenuBar: true,
-		webPreferences: {
-			nodeIntegration: false,
-			preload: path.join(__dirname, 'browser.js'),
-			plugins: true
-		}
-	});
+  const lastWindowState = config.get('lastWindowState');
+  const win = new electron.BrowserWindow({
+    title: app.getName(),
+    show: false,
+    x: lastWindowState.x,
+    y: lastWindowState.y,
+    width: lastWindowState.width,
+    height: lastWindowState.height,
+    icon: process.platform === 'linux' && path.join(__dirname, 'static', 'Icon.png'),
+    minWidth: 400,
+    minHeight: 200,
+    titleBarStyle: 'hidden-inset',
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false,
+      preload: path.join(__dirname, 'browser.js'),
+      plugins: true
+    }
+  });
 
-	if (process.platform === 'darwin') {
-		win.setSheetOffset(40);
-	}
+  if (process.platform === 'darwin') {
+    win.setSheetOffset(40);
+  }
 
-	win.loadURL('https://trello.com/');
+  win.loadURL('https://trello.com/');
 
-	win.on('close', e => {
-		if (!isQuitting) {
-			e.preventDefault();
+  win.on('close', e => {
+    if (isQuitting) {
+      if (!mainWindow.isFullScreen()) {
+        config.set('lastWindowState', mainWindow.getBounds());
+      }
+    } else {
+      e.preventDefault();
 
-			if (process.platform === 'darwin') {
-				app.hide();
-			} else {
-				app.quit();
-			}
-		} else {
-			if (!mainWindow.isFullScreen()) {
-				config.set('lastWindowState', mainWindow.getBounds());
-			}
-		}
+      if (process.platform === 'darwin') {
+        app.hide();
+      } else {
+        app.quit();
+      }
+    }
+  });
 
-	});
-
-	return win;
+  return win;
 }
 
 app.on('ready', () => {
-	mainWindow = createMainWindow();
-	const page = mainWindow.webContents;
+  mainWindow = createMainWindow();
+  const page = mainWindow.webContents;
 
-	page.on('dom-ready', () => {
-		page.insertCSS(fs.readFileSync(path.join(__dirname, 'browser.css'), 'utf8'));
-		mainWindow.show();
-	});
+  page.on('dom-ready', () => {
+    page.insertCSS(fs.readFileSync(path.join(__dirname, 'browser.css'), 'utf8'));
+    mainWindow.show();
+  });
 
-	page.on('new-window', (e, url) => {
-		e.preventDefault();
-		electron.shell.openExternal(url);
-	});
+  page.on('new-window', (e, url) => {
+    e.preventDefault();
+    electron.shell.openExternal(url);
+  });
 
-	mainWindow.webContents.session.on('will-download', (event, item) => {
-		const totalBytes = item.getTotalBytes();
+  mainWindow.webContents.session.on('will-download', (event, item) => {
+    const totalBytes = item.getTotalBytes();
 
-		item.on('updated', () => {
-			mainWindow.setProgressBar(item.getReceivedBytes() / totalBytes);
-		});
+    item.on('updated', () => {
+      mainWindow.setProgressBar(item.getReceivedBytes() / totalBytes);
+    });
 
-		item.on('done', (e, state) => {
-			mainWindow.setProgressBar(-1);
+    item.on('done', (e, state) => {
+      mainWindow.setProgressBar(-1);
 
-			if (state === 'interrupted') {
-				electron.Dialog.showErrorBox('Download error', 'The download was interrupted');
-			}
-		});
-	});
+      if (state === 'interrupted') {
+        electron.Dialog.showErrorBox('Download error', 'The download was interrupted');
+      }
+    });
+  });
 
-	const template = [{
-		label: 'Application',
-		submenu: [
-			{label: 'About Application', selector: 'orderFrontStandardAboutPanel:'},
-			{type: 'separator'},
-			{
-				label: 'Quit', accelerator: 'Command+Q', click: () => {
-				app.quit();
-			}
-			}
-		]
-	}, {
-		label: 'Edit',
-		submenu: [
-			{label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:'},
-			{label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:'},
-			{type: 'separator'},
-			{label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:'},
-			{label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:'},
-			{label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:'},
-			{label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:'}
-		]
-	}
-	];
+  const template = [{
+    label: 'Application',
+    submenu: [
+      {label: 'About Application', selector: 'orderFrontStandardAboutPanel:'},
+      {type: 'separator'},
+      {
+        label: 'Quit', accelerator: 'Command+Q', click: () => {
+          app.quit();
+        }
+      }
+    ]
+  }, {
+    label: 'Edit',
+    submenu: [
+      {label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:'},
+      {label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:'},
+      {type: 'separator'},
+      {label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:'},
+      {label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:'},
+      {label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:'},
+      {label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:'}
+    ]
+  }
+  ];
 
-	electron.Menu.setApplicationMenu(electron.Menu.buildFromTemplate(template));
+  electron.Menu.setApplicationMenu(electron.Menu.buildFromTemplate(template));
 });
 
 app.on('window-all-closed', () => {
-	app.quit();
+  app.quit();
 });
 
 app.on('activate', () => {
-	mainWindow.show();
+  mainWindow.show();
 });
 
 app.on('before-quit', () => {
-	isQuitting = true;
+  isQuitting = true;
 
-	if (!mainWindow.isFullScreen()) {
-		config.set('lastWindowState', mainWindow.getBounds());
-	}
+  if (!mainWindow.isFullScreen()) {
+    config.set('lastWindowState', mainWindow.getBounds());
+  }
 });
